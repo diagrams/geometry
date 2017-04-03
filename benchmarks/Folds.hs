724a5f4324cc92733f99cff423bfddf82698c918
Christopher Chalmers <c.chalmers@me.com>
2017-04-03 22:45:57 +0100

    Use dragon8 instead of dragon10 in folds benchmark

--- benchmarks/Folds.hs
+++ benchmarks/Folds.hs
@@ -15,7 +15,7 @@ import Samples
 main :: IO ()
 main = do
   let path10 = toPath . take 10
-  let !dragon10  = fromOffsets (dragonOffsets 10) :: Path V2 Double
+  let !dragon8  = fromOffsets (dragonOffsets 8) :: Path V2 Double
       !squares10 = path10 (randomSquares 0)
       !rects10   = path10 (randomRects 1)
       !circles10 = path10 (randomCircles 2)
@@ -24,7 +24,7 @@ main = do
   let trace = traceV origin (V2 0.4 0.7)
   defaultMain
     [ bgroup "bounding-box"
-      [ bench "dragon-10"   $ whnf (diameter unitX) dragon10
+      [ bench "dragon-8"   $ whnf (diameter unitX) dragon8
       , bench "squares-10"  $ whnf (diameter unitX) squares10
       , bench "rects-10"    $ whnf (diameter unitX) rects10
       , bench "circles-10"  $ whnf (diameter unitX) circles10
@@ -32,7 +32,7 @@ main = do
       , bench "triangles-10" $ whnf (diameter unitX) triangles10
       ]
     , bgroup "trace"
-      [ bench "dragon-10"   $ nf trace dragon10
+      [ bench "dragon-8"   $ nf trace dragon8
       , bench "squares-10"  $ nf trace squares10
       , bench "rects-10"    $ nf trace rects10
       , bench "circles-10"  $ nf trace circles10

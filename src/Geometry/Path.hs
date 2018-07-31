689af5edc74dadbc651285da0ba3c5e06843afb8
Brent Yorgey <byorgey@gmail.com>
2018-07-30 23:13:09 -0500

    G.Path: fix pattern-match warning
    Use :<| (provided by Data.Sequence) instead of :< provided by lens.
    The latter does seem to work but confuses the pattern match
    completeness checker, which thinks it will never match.

--- src/Geometry/Path.hs
+++ src/Geometry/Path.hs
@@ -174,8 +174,8 @@ pathEnv i0 (Path ts) = \v -> F.foldl' (\i t -> hull (locTrailEnv t v) i) i0 ts
 
 pathEnvelope :: (Metric v, OrderedField n) => Path v n -> Envelope v n
 pathEnvelope = \case
-  Path (t :< ts) -> Envelope $ \(Dir v) -> pathEnv (locTrailEnv t v) (Path ts) v
-  _              -> EmptyEnvelope
+  Path (t :<| ts) -> Envelope $ \(Dir v) -> pathEnv (locTrailEnv t v) (Path ts) v
+  _               -> EmptyEnvelope
 -- This could be defined as @foldMap getEnvelope ts@ but this would be
 -- less efficient.
 {-# SPECIALISE pathEnvelope :: Path V2 Double -> Envelope V2 Double #-}
